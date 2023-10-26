21873a89d096a615dc256579bb72efe10f91e31e
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-10-26 22:42:31 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+package days18;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        {
+            int myNum = 5;            
+    float myFloatNum = 5.99f;    
+    char myLetter = 'L';         
+    boolean myBool = true;       
+    String myText = "false";        
+    System.out.println(myNum) ;
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+
+} 
+
+    }
+}
